744827e4cd5e8c4223adc1be9364f11335ad3b44
adarraji <a.darraji@outlook.com>
2023-08-08 19:30:02 -0400

    menu and navbar components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Menu from "./Menu"
+import Link from "next/link"
 
 const Navbar = () => {
     return (
-        <div >Navbar</div>
+        <div >
+            {/* LOGO */}
+            <div>
+                <Link href="#">
+                    Massimo
+                </Link>
+            </div>
+            {/* MOBILE MENU */}
+            <div>
+                <Menu />
+            </div>
+        </div>
     )
 }
 
