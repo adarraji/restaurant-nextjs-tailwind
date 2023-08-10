0eb4a465ea9a7f05fbb3b5062212d8040240f3f8
adarraji <a.darraji@outlook.com>
2023-08-09 21:18:11 -0400

    updated navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 const Navbar = () => {
     return (
-        <div >
+        <div className="h-12 text-red-500 p-4 items-center flex justify-between border-b-2 border-b-red-500" >
             {/* LOGO */}
             <div>
                 <Link href="/">
