4eb4b95cddad759c1dda7e9fbfd9058e735a880b
adarraji <a.darraji@outlook.com>
2023-08-12 13:19:52 -0400

    updated navbar 2734

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,11 @@ import Link from "next/link"
 const Navbar = () => {
     return (
         <div className="h-12 text-red-500 p-4 items-center flex justify-between border-b-2 border-b-red-500 uppercase" >
+            <div className="hidden md:flex gap-4">
+                <Link href="/">Home page</Link>
+                <Link href="/menu">Home page</Link>
+                <Link href="/">Contact</Link>
+            </div>
             {/* LOGO */}
             <div className="text-xl">
                 <Link href="/">
@@ -12,7 +17,7 @@ const Navbar = () => {
                 </Link>
             </div>
             {/* MOBILE MENU */}
-            <div>
+            <div className="md:hidden">
                 <Menu />
             </div>
         </div>
