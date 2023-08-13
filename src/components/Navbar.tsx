e44df85e84db27804fb53380e81af3391ea55b5b
adarraji <a.darraji@outlook.com>
2023-08-13 19:56:22 -0400

    added right links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Menu from "./Menu"
 import Link from "next/link"
+import CartIcon from "./CartIcon"
+import Image from "next/image"
 
 const Navbar = () => {
+    const user = false
     return (
         <div className="h-12 text-red-500 p-4 items-center flex justify-between border-b-2 border-b-red-500 uppercase" >
+            {/* LEFT LINKS */}
             <div className="hidden md:flex gap-4">
                 <Link href="/">Home page</Link>
                 <Link href="/menu">Home page</Link>
@@ -19,6 +23,17 @@ const Navbar = () => {
             {/* MOBILE MENU */}
             <div className="md:hidden">
                 <Menu />
+            </div>
+            {/* RIGHT LINKS */}
+            <div className="hidden md:flex gap-4 items-center">
+                <div className="flex gap-2 items-center cursor-pointer bg-orange-300 px-1 rounded-md">
+                    <Image src="/phone.png" alt="" width={20} height={20} />
+                    <span>123 456 78</span>
+                </div>
+                {!user
+                    ? <Link href="/login">Login</Link>
+                    : <Link href="/orders">Orders</Link>}
+                <CartIcon />
             </div>
         </div>
     )
