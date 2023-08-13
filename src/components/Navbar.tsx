6540214448caff5c48a34bd622a3d02e1259edd9
adarraji <a.darraji@outlook.com>
2023-08-13 19:57:02 -0400

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
             </div>
             {/* RIGHT LINKS */}
             <div className="hidden md:flex gap-4 items-center">
-                <div className="flex gap-2 items-center cursor-pointer bg-orange-300 px-1 rounded-md">
+                <div className="flex gap-2 items-center cursor-pointer bg-orange-300 px-1 rounded-md ">
                     <Image src="/phone.png" alt="" width={20} height={20} />
                     <span>123 456 78</span>
                 </div>
