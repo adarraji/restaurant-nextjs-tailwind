d6da03c3ecc7271b3085c8a56ac2d7ad472bdbc5
adarraji <a.darraji@outlook.com>
2023-08-16 16:46:12 -0400

    update countdown code

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,13 +1,39 @@
 "use client"
-import React from 'react'
-import Countdown from 'react-countdown'
-
-const endingDate = new Date("2023-09-25")
+import React, { useState, useEffect } from "react";
 
 const CountDown = () => {
-    return (
-        <Countdown className='font-bold text-5xl text-yellow-300' date={endingDate} />
-    )
-}
 
-export default CountDown
+    let difference = +new Date(`10/10/2023`) - +new Date();
+    const [delay, setDelay] = useState(difference);
+    const [days, setDays] = useState(0)
+    const [hours, setHours] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+    const [seconds, setSeconds] = useState(0)
+
+    useEffect(() => {
+        // SETTING THE VALUES INSIDE USEEFFECT BECAUSE OF NEXT HYDRATION ISSUE        
+        setDays(Math.floor(difference / (1000 * 60 * 60 * 24)))
+        setHours(Math.floor((difference / (1000 * 60 * 60)) % 24))
+        setMinutes(Math.floor((difference / 1000 / 60) % 60))
+        setSeconds(Math.floor((difference / 1000) % 60))
+
+        const timer = setInterval(() => {
+            setDelay(delay - 1);
+        }, 1000);
+
+        if (delay === 0) {
+            clearInterval(timer);
+        }
+
+        return () => {
+            clearInterval(timer);
+        };
+    });
+    return (
+        <span className="font-bold text-5xl text-yellow-300">
+            {days}:{hours}:{minutes}:{seconds}
+        </span>
+    );
+};
+
+export default CountDown;
