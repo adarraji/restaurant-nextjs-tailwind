289f3a259a81222728b01319a5f9f8f676a920e4
adarraji <a.darraji@outlook.com>
2023-09-11 22:19:11 -0400

    20758

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const OrdersPage = () => {
     return (
-        <div>OrdersPage</div>
+        <div className="p-4 lg:px-20 xl:px-40">
+            <table className="w-full">
+                <thead>
+                    <tr className="text-left">
+                        <th className="hidden md:block">Order ID</th>
+                        <th>Date</th>
+                        <th>Price</th>
+                        <th className="hidden md:block">Products</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="hidden md:block">1237861238721</td>
+                        <td>19.07.2023</td>
+                        <td>89.90</td>
+                        <td className="hidden md:block">Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+                        <td>On the way (approx. 10min)...</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
     )
 }
 
