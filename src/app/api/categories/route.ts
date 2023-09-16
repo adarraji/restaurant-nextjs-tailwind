0b196a0d774862d22e913516bbfa9f216b05d708
adarraji <a.darraji@outlook.com>
2023-09-16 12:14:01 -0400

    get categories

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,5 +1,21 @@
+import { PrismaClient } from "@prisma/client"
 import { NextResponse } from "next/server"
 
-export const GET = () => {
+const prisma = new PrismaClient()
+
+// GET ALL CATEGORIRES
+export const GET = async () => {
+    try {
+        const categories = await prisma.category.findMany()
+        return new NextResponse(JSON.stringify(categories), { status: 200 })
+    } catch (err) {
+        console.log(err)
+        return new NextResponse(JSON.stringify({ message: "Something went wrong!" }), { status: 500 })
+    }
+
+}
+
+export const POST = () => {
     return new NextResponse("Hello", { status: 200 })
 }
+
