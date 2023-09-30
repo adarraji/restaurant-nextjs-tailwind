2960016f358b0c3dc7ac85362b56dab7bf440bd2
adarraji <a.darraji@outlook.com>
2023-09-29 22:22:44 -0400

    getting isadmin and email from token

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -6,6 +6,9 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 export const authOptions: NextAuthOptions = {
 
     adapter: PrismaAdapter(prisma),
+    session: {
+        strategy: "jwt"
+    },
     providers: [
         GoogleProvider({
             // clientId: process.env.GOOGLE_ID as string,
@@ -13,7 +16,30 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GOOGLE_ID!,
             clientSecret: process.env.GOOGLE_SECRET!,
         }),
-    ]
+    ],
+    callbacks: {
+        async session({ token, session }) {
+            if (token) {
+                session.user.isAdmin = token.isAdmin
+            }
+
+            return session
+        },
+
+        async jwt({ token }) {
+
+            const userInDb = await prisma.user.findUnique({
+                where: {
+                    email: token.email!
+                }
+            })
+
+            token.isAdmin = userInDb?.isAdmin!
+
+            return token
+        },
+
+    }
 }
 
 export const getAuthSession = () => getServerSession(authOptions)
