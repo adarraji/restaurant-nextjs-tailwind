c3caf1a68011fe0f6ca5391679100dae9db70696
adarraji <a.darraji@outlook.com>
2023-10-09 17:06:20 -0400

    added product id route

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,25 @@
+import prisma from "@/utils/connect"
+import { NextRequest, NextResponse } from "next/server"
+
+type ParamsType = {
+    id: string
+}
+
+// GET SINGLE PRODUCT
+export const GET = async (req: NextRequest, { params }: { params: ParamsType }) => {
+    const { id } = params
+
+    try {
+        const product = await prisma.product.findUnique({
+            where: {
+                id: id,
+            },
+        })
+        return new NextResponse(JSON.stringify(product), { status: 200 })
+
+    } catch (err) {
+        console.log(err)
+        return new NextResponse(JSON.stringify({ message: "Something went wrong" }), { status: 500 })
+    }
+
+}
