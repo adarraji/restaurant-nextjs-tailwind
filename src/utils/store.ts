dbd7614614e3085478756332a1d22422947e712d
adarraji <a.darraji@outlook.com>
2023-10-14 17:59:36 -0400

    fix

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -28,37 +28,3 @@ export const useCartStore = create<CartType & ActionTypes>((set, get) => ({
         }))
     },
 }))
-
-// export const useCartStore = create< CartType & ActionTypes >((set, get) => ({
-//     peoducts: INITIAL_STATE.products,
-//     totlaItems: INITIAL_STATE.totlaItems,
-//     totalPrice: INITIAL_STATE.totalPrice,
-//     addTocart(item) {
-//         set((state) => ({
-//             peoducts: [...state.products, item],
-//             totalItems: state.totalItems + item.quantitiy,
-//             totalPrice: state.totalPrice + item.price,
-//         }))
-//     },
-//     removeFromCart(item) {
-//         set((state) => ({
-//             peoducts: state.products.filter(product => product.id !== item.id),
-//             totalItems: state.totalItems - item.quantitiy,
-//             totalPrice: state.totalPrice + -item.price
-//         }))
-//     },
-// }))
-
-// export const useCaerStore = create<CartType & ActionType>((get, set) => ({
-//     peoducts: INITITAL_STATE.products,
-//     totlaItems: INITITAL_STATE.totlaItems,
-//     totalPrice: INITITAL_STATE.totalPrice,
-//     addTocart(item) {
-//         set(state) => ({
-//             products: ...state.products,
-//             quantitiy: state.totalItems + item.quantitiy,
-//             totalPrice: state.totalPrice + item.price
-//         })
-//     }
-
-// }))
