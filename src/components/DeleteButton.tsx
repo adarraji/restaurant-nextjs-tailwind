376d9349e9649a67b295b147ff3bd203c84676de
adarraji <a.darraji@outlook.com>
2023-10-15 19:35:44 -0400

    created delete button component

---
+++ src/components/DeleteButton.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Image from "next/image"
+
+const DeleteButton = () => {
+    return (
+        <button className="bg-red-400 p-2 rounded-full absolute top-4 right-4">
+            <Image src="/delete.png" width={20} height={20} />
+        </button>
+    )
+}
+
+export default DeleteButton
