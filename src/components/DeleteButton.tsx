8a62a771fd7fbe0f32bad45de8e50c7227f9f618
adarraji <a.darraji@outlook.com>
2023-10-15 19:37:14 -0400

    fix

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 const DeleteButton = () => {
     return (
         <button className="bg-red-400 p-2 rounded-full absolute top-4 right-4">
-            <Image src="/delete.png" width={20} height={20} />
+            <Image src="/delete.png" alt="" width={20} height={20} />
         </button>
     )
 }
