9dd9162d4668e510159a4e708ceaa5810dad84f7
adarraji <a.darraji@outlook.com>
2023-10-17 15:12:29 -0400

    added make payment request func

--- src/app/pay/[id]/page.tsx
+++ src/app/pay/[id]/page.tsx
@@ -1,7 +1,27 @@
 "use client"
 
+import { useEffect } from "react"
+
 const PaymentPage = ({ params }: { params: { id: string } }) => {
     const { id } = params
+
+    useEffect(() => {
+        const makeRequest = async () => {
+            try {
+                const res = await fetch(`http://localhost:3000/api/create-intent/${id}`, {
+                    method: "POST"
+                })
+                const data = res.json()
+
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        makeRequest()
+
+    }, [id])
+
     return (
         <div>PaymentPage</div>
     )
