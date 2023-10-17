faaf69f371903db9f015c47626c2d6dd51250910
adarraji <a.darraji@outlook.com>
2023-10-17 13:11:46 -0400

    handle checkout

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,14 +1,42 @@
 "use client"
 import { useCartStore } from "@/utils/store"
+import { useSession } from "next-auth/react"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 import React, { useEffect } from 'react'
 
 const CartPage = () => {
     const { products, totalItems, totalPrice, removeFromCart } = useCartStore()
+    const { data: session } = useSession()
+    const router = useRouter()
 
     useEffect(() => {
         useCartStore.persist.rehydrate()
     }, [])
+
+    const handleChekout = async () => {
+        if (!session) {
+            router.push("/")
+        } else {
+            try {
+                const res = await fetch("https://localhost:3000/api/orders", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        price: totalPrice,
+                        products,
+                        status: "Not Paid!",
+                        userEmail: session.user.email
+                    })
+                })
+                const data= await res.json()
+                router.push(`/pay/${data.id}`)
+
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    }
 
     return (
         <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
@@ -49,7 +77,7 @@ const CartPage = () => {
                     <span className="">TOTAL(INCL. VAT)</span>
                     <span className="font-bold">{totalPrice}</span>
                 </div>
-                <button className="bg-red-500 text-white p-3 rounded w-1/2 self-end">CHECKOUT</button>
+                <button className="bg-red-500 text-white p-3 rounded w-1/2 self-end" onClick={handleChekout}>CHECKOUT</button>
             </div>
         </div>
     )
