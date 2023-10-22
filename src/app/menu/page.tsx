085325e2b8a393f4f5f732210fd46e58c1265a68
adarraji <a.darraji@outlook.com>
2023-10-22 19:58:19 -0400

    check if menu data avaialble

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -15,7 +15,7 @@ const MenuPage = async () => {
     const menu: MenuType = await getData()
     return (
         <div className="p-4 lg:px-20 xl:px40 h-[calc(100vh-6rem)]  md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
-            {menu.map(category => (
+            {menu && menu.map(category => (
                 <Link href={`/menu/${category.slug}`} key={category.id} className="w-full h-1/3 bg-cover p-8 md:h-1/2" style={{ backgroundImage: `url(${category.img})` }}>
                     <div className={`text-${category.color} w-1/2`}>
                         <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
