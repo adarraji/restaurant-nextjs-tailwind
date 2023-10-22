234268258373b46467ebbe2a57134f513000031d
adarraji <a.darraji@outlook.com>
2023-10-22 19:36:19 -0400

    fix

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -40,7 +40,7 @@ const AddPage = () => {
     return <p>Loading...</p>;
   }
 
-  if (status === "unauthenticated") {
+  if (status === "unauthenticated" || !session?.user.isAdmin) {
     return
   }
 
