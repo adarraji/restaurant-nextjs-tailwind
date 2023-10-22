43ceb0976f393bbb751d92b53265a8c09f800e16
adarraji <a.darraji@outlook.com>
2023-10-22 14:41:44 -0400

    Changed name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ import "react-toastify/dist/ReactToastify.css"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Massimo',
+  title: 'Bistro Pro ',
   description: 'Best food in town!',
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
             {/* LOGO */}
             <div className="text-xl md:font-bold flex-1 md:text-center">
                 <Link href="/">
-                    Massimo
+                    Bistro Pro
                 </Link>
             </div>
             {/* MOBILE MENU */}
